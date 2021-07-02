736d89390b24e9afadd9793e6f01b6f876ff0978
amg1551 <you@example.com>
2021-07-02 16:10:21 +0300

    second

---
+++ pages/dex.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>site içinde site</title>
+        <link rel="icon" href="/indir.jpg" />
+      </Head>
+
+      <main className={styles.main}>
+
+        
+        
+        
+        
+       <body>
+
+
+
+  <iframe src="/index.html" allowtransparency="true" width="1500" height="800" frameborder="0" scrolling="no" allowfullscreen></iframe>
+
+ 
+      </body>
+        
+      </main>
+
+
+    </div>
+  );
+
+}
